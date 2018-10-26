67a5425f9a16ef3f044a8304b6081acd9bbf020d
Sylwia <sylviaskovron@gmail.com>
2018-10-26 10:00:09 +0200

    Updates

---
+++ precache-manifest.fc3ff0458d17b26af73b98a4bc8c0806.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "92256be25dedcbda10b03aecde3eccd9",
+    "url": "/simple-react-onlineshop/static/media/scarf1.92256be2.png"
+  },
+  {
+    "revision": "54d641337ab043605686095c35c9f3a5",
+    "url": "/simple-react-onlineshop/static/media/pants5.54d64133.png"
+  },
+  {
+    "revision": "0877d2b8e743193afc44",
+    "url": "/simple-react-onlineshop/static/js/runtime~main.0877d2b8.js"
+  },
+  {
+    "revision": "08178c00833db723513c",
+    "url": "/simple-react-onlineshop/static/js/main.08178c00.chunk.js"
+  },
+  {
+    "revision": "f631fc15c30efcd8fafe",
+    "url": "/simple-react-onlineshop/static/js/1.f631fc15.chunk.js"
+  },
+  {
+    "revision": "08178c00833db723513c",
+    "url": "/simple-react-onlineshop/static/css/main.827117bb.chunk.css"
+  },
+  {
+    "revision": "408a287c6b90b41d4fd8f6df5636d4ed",
+    "url": "/simple-react-onlineshop/index.html"
+  }
+];
